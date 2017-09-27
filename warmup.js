be94cd8c304eeb7acd2a40e7e7074212d3a47efb
KaiserChan <chankaiserkk@gmail.com>
2017-09-26 22:10:35 -0400

    updated exercise ... created warmup.rb and warmup.js for warmup ...

---
+++ warmup.js
@@ -0,0 +1,24 @@
+// 1
+var apples = 14;
+console.log(apples);
+console.log('I have ' + apples + ' apples.');
+
+
+// 2
+var materials = ['wood', 'metal', 'stone'];
+var words = {
+  'elephant': "The world's largest land mammal.",
+  'school': 'A place of learning.',
+  'ice cream': 'A delicious milk-based dessert.'
+};
+
+
+// 3
+var num = 16;
+if (num > 10) {
+  console.log(num + ' is greater than 10.');
+} else if (num === 10) {
+  console.log(num + ' is exactly 10.');
+} else {
+  console.log(num + ' is less than 10.');
+}
